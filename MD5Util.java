f67b56fcac35e3f9daa0c2b7364d8623a6a093fc
yl <yanlai@users.noreply.github.com>
2016-10-18 14:42:11 +0800

    Update MD5Util.java

--- MD5Util.java
+++ MD5Util.java
@@ -4,19 +4,16 @@ public  static String changeMD5(String s) {
 	            byte[] btInput = s.getBytes();
 	            // 获得MD5摘要算法的 MessageDigest 对象
 	            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-	            // 使用指定的字节更新摘要
-	            mdInst.update(btInput);
-	            // 获得密文
-	            byte[] md = mdInst.digest();
-	            // 把密文转换成十六进制的字符串形式
-	            int j = md.length;
-	            char str[] = new char[j * 2];
-	            int k = 0;
-	            for (int i = 0; i < j; i++) {
-	                byte byte0 = md[i];
-	                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-	                str[k++] = hexDigits[byte0 & 0xf];
-	            }
+	            md.update(source);      //更新摘要  
+		    byte tmp[]=md.digest(); //进行md5计算  
+		    char str[] =new char[ 16*2 ]; //md的结果是128位的长整数，用16进制表示的话就是32位，即32个字符表示  
+
+		    int k = 0;  
+		    for( int i=0 ; i<16; i++){  
+			byte byte0 =tmp[i];  
+			str[k++] = hexDigits[ byte0 >>> 4 & 0xf];//低四位与1111做逻辑与运算 得到16进制数存入低位  
+			str[k++] = hexDigits[ byte0 & 0xf];     //高四位与1111做逻辑与运算得到16进制存入高位  
+		    }  
 	            return new String(str);
 	        } catch (Exception e) {
 	            e.printStackTrace();
